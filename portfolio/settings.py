4af792bdbb156ab42d4bb1c1082a67d7c8355644
unknown <vermashivam0606@gmail.com>
2020-07-25 04:22:58 -0700

    surity

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 from decouple import config 
 
@@ -135,4 +136,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 
-
+django_heroku.settings(locals())
