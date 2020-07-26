21db358360dcb2362d09ff8d564638304fa4920a
Shivam Verma <55542058+The-Nightwing@users.noreply.github.com>
2020-07-26 02:04:41 -0700

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -61,7 +61,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(SETTINGS_PATH,'templates'),
+            os.path.join(BASE_DIR,'templates'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
