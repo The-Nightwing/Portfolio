e136ec6c2ed314181ff898f6812a692ca267b863
Shivam Verma <55542058+The-Nightwing@users.noreply.github.com>
2020-07-26 08:01:01 -0700

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -15,7 +15,6 @@ from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-SETTINGS_PATH = os.path.normpath(os.path.dirname(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -63,7 +62,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR,'templates'),
+            os.path.join(BASE_DIR,'/templates/'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
