f064bf6f2fb7730fff114f63751a27bf4dabfe3c
Shivam Verma <55542058+The-Nightwing@users.noreply.github.com>
2020-07-26 10:19:14 -0700

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 from decouple import config 
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -133,5 +132,5 @@ STATICFILES_DIRS = (
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-django_heroku.settings(locals())
+
 
