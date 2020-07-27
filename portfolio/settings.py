497cfc48c1f5a0e70e44810e1b7e998db556bc39
Shivam <vermashivam0606@gmail.com>
2020-07-27 16:24:04 +0530

    update

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -58,11 +58,10 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'portfolio.urls'
 
 
-
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [ os.path.join(BASE_DIR, '/templates/'), ],
+        'DIRS': [ 'templates' ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
