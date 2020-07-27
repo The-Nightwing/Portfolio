66e10b98f07b110c6b902c9dd7a3e102b968ecaa
Shivam <vermashivam0606@gmail.com>
2020-07-27 16:54:47 +0530

    update

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main'
 ]
 
 MIDDLEWARE = [
@@ -61,7 +62,7 @@ ROOT_URLCONF = 'portfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [ r'c:\Users\verma\Desktop\project\Portfolio-01\templates' ],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
