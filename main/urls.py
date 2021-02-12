6283c8a3c9df3df8b651eb2f7601873b79b88cba
shivam verma <vermashivam0606@gmail.com>
2021-02-13 03:04:27 +0530

    Organized Projects Data

--- main/urls.py
+++ main/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('',views.index, name="index"),
     path('project/',views.projects,name="projects"),
     path('skills/',views.skills,name="skills"),
-    path('looc/',views.life,name="life")
+    path('looc/',views.life,name="life"),
+    path('work_experience',views.work_experience,name="wexp")
 ]
